2f81ccfb2e84a229aab6f2a829ca0dc7017063eb
Civel-1 <jules.civel@gmail.com>
2018-04-03 21:58:19 +0200

    US_01

---
+++ app/src/main/java/com/example/cbs/cbs/AccueilActivity.java
@@ -0,0 +1,13 @@
+package com.example.cbs.cbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccueilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+}
