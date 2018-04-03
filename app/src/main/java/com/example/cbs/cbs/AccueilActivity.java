a99de017e999032b1449fac8548623635a5bee34
Civel-1 <jules.civel@gmail.com>
2018-04-04 00:32:39 +0200

    US_03_Version1

--- app/src/main/java/com/example/cbs/cbs/AccueilActivity.java
+++ app/src/main/java/com/example/cbs/cbs/AccueilActivity.java
@@ -1,7 +1,10 @@
 package com.example.cbs.cbs;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        EditText inputNumero= findViewById(R.id.inputNumber);
+
+        if(prefs!=null){
+
+            inputNumero.setText(prefs.getString("defaultNum", "RIEN")+" "+ prefs.getString("defaultName", "RIEN"));
+        }
     }
 }
