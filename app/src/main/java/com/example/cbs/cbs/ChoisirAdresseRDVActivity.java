96e48f57d26a6daae2197d15197a5a88f522c1e4
GwendalBrossard <brossard.gwendal@gmail.com>
2018-04-05 23:11:24 +0200

    Ajout d'une carte google map pour pouvoir sélectionner une adresse avec un clic sur la carte (à finir)

--- app/src/main/java/com/example/cbs/cbs/ChoisirAdresseRDVActivity.java
+++ app/src/main/java/com/example/cbs/cbs/ChoisirAdresseRDVActivity.java
@@ -1,18 +1,26 @@
 package com.example.cbs.cbs;
 
+import android.arch.lifecycle.ViewModelStoreOwner;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ChoisirAdresseRDVActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private String contactsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,11 @@ public class ChoisirAdresseRDVActivity extends FragmentActivity implements OnMap
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Récupère la liste des contacts passée par l'activity RenseignerNumeroActivity
+        Intent intent = getIntent();
+        contactsList = intent.getStringExtra("contactsList");
+        Log.i("contactsList", "ChoisirAdresseRDVActivity -> contactsList : " + contactsList);
     }
 
 
@@ -35,12 +48,40 @@ public class ChoisirAdresseRDVActivity extends FragmentActivity implements OnMap
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //Permet d'ajouter un marker sur la map lorsque l'on clique dessus
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+
+                Marker markePointRDV = mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("title")
+                        .snippet("snippet"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                markePointRDV.showInfoWindow();
+
+                //TODO Transformer une latitude et un longitude en une adresse grace à l'API Google
+                /**
+                 *  Permet de connaitre une adresse à partir d'une latitude et d'une longitude : https://developers.google.com/maps/documentation/geocoding/start?hl=fr
+                 *  https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLng.latitude + "," + latLng.longitude + "&key=AIzaSyDAX7lTjLLLk1bA1uf6mZaRDfThjhMiCJA
+                 */
+            }
+        });
+
+
+        //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDAX7lTjLLLk1bA1uf6mZaRDfThjhMiCJA
+
     }
+
+    public void validerAdresse(View v)
+    {
+        //TODO gérer la validation de l'adresse
+        Log.i("validerAdresse", "bouton appuyé");
+    }
+
 }
