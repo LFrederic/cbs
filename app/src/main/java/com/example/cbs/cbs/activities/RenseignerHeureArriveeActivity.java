3a81086d22e9fd6c727030eade5bd96254330ebb
GwendalBrossard <brossard.gwendal@gmail.com>
2018-04-12 11:54:59 +0200

    Réparation des conneries de Joan

--- app/src/main/java/com/example/cbs/cbs/activities/RenseignerHeureArriveeActivity.java
+++ app/src/main/java/com/example/cbs/cbs/activities/RenseignerHeureArriveeActivity.java
@@ -15,8 +15,11 @@ import android.widget.Toast;
 import com.example.cbs.cbs.R;
 import com.example.cbs.cbs.services.SmsService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class RenseignerHeureArriveeActivity extends AppCompatActivity {
 
@@ -51,9 +54,8 @@ public class RenseignerHeureArriveeActivity extends AppCompatActivity {
         int mMonth = c.get(Calendar.MONTH); // current month
         int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
         // On ouvre le calendrier
-        DatePickerDialog datePickerDialog = new DatePickerDialog(RenseignerHeureArriveeActivity.this,
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(RenseignerHeureArriveeActivity.this,
                 new DatePickerDialog.OnDateSetListener() {
-
                     @Override
                     public void onDateSet(DatePicker view, int year,
                                           int monthOfYear, int dayOfMonth) {
@@ -65,26 +67,92 @@ public class RenseignerHeureArriveeActivity extends AppCompatActivity {
                         annee = year;
                     }
                 }, mYear, mMonth, mDay);
+        datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
         datePickerDialog.show();
     }
 
     public void choisirHeure(View view)
     {
+
+        edtDatePicker = (EditText) findViewById(R.id.edtDatePicker);
+
+        //Verifications des conditions
+        if (edtDatePicker.getText().toString().isEmpty())
+        {
+            Toast.makeText(RenseignerHeureArriveeActivity.this, "Veuillez d'abord choisir une date pour le rendez-vous.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        //Ouverture du timePicker + récupération des données
         edtTimePicker = findViewById(R.id.edtTimePicker);
         Calendar mcurrentTime = Calendar.getInstance();
         int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
         int minute = mcurrentTime.get(Calendar.MINUTE);
-        TimePickerDialog mTimePicker;
+        final TimePickerDialog mTimePicker;
         mTimePicker = new TimePickerDialog(RenseignerHeureArriveeActivity.this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                edtTimePicker.setText( selectedHour + ":" + selectedMinute);
-                heure = selectedHour;
-                minutes = selectedMinute;
+                String dtStart = edtDatePicker.getText().toString();
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                try
+                {
+                    Date date = format.parse(dtStart);
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(date);
+                    int selectedDay = cal.get(Calendar.DAY_OF_MONTH);
+
+                    if (isPastDate(selectedDay, selectedHour, selectedMinute))
+                    {
+                        edtTimePicker.setText("");
+                        return;
+                    }
+
+
+
+                }
+                catch (ParseException e)
+                {
+                    e.printStackTrace();
+                    Toast.makeText(RenseignerHeureArriveeActivity.this, "Erreur dans la date.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+
+                    edtTimePicker.setText( selectedHour + ":" + selectedMinute);
+                    heure = selectedHour;
+                    minutes = selectedMinute;
             }
         }, hour, minute, true);//Yes 24 hour time
         mTimePicker.setTitle("Choisir l'heure");
         mTimePicker.show();
+    }
+
+
+    private boolean isPastDate(int selectedDay, int selectedHour, int selectedMinute)
+    {
+        boolean isPastDate = false;
+
+        int jourActuel = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        int heureActuelle = Calendar.getInstance().get(Calendar.HOUR);
+        int minutesActuelles = Calendar.getInstance().get(Calendar.MINUTE);
+
+        System.out.println("selectedday : " + selectedDay + " - jour actuel : " + jourActuel);
+        System.out.println("heure actuelle : " + heureActuelle + " - minute acutelle : " + minutesActuelles);
+        System.out.println("heure selected : " + selectedHour + " - minute selected : " + selectedMinute);
+
+        if (selectedDay == jourActuel)
+        {
+            System.out.println("selectedDay == jourActuel");
+            if (heureActuelle == selectedHour && selectedMinute <  minutesActuelles || selectedHour < heureActuelle)
+            {
+                System.out.println("heureActuelle == selectedHour && minutesActuelles < selectedMinute | heureActuelle < selectedHour");
+                Toast.makeText(RenseignerHeureArriveeActivity.this, "Vous ne pouvez pas choisir une date passée.", Toast.LENGTH_LONG).show();
+                isPastDate = true;
+            }
+        }
+
+        return  isPastDate;
     }
 
     public void validerHeureArrivee(View view)
